74bc6e62fa69b4d2f1b69122d1b9d83917644d26
Kazuya Takeshima <mail@mitukiii.jp>
2013-01-21 01:54:44 +0900

    Add user:list task.

---
+++ lib/agharta/tasks/user_list.rb
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+module Agharta
+  module Tasks
+    class UserList < Thor::Group
+      Tasks.register 'user:list', self
+
+      def self.banner
+        'agharta user:list'
+      end
+
+      include Actions
+
+      desc 'show user list'
+
+      def setup
+        @users = env.config[:twitter] || {}
+        @default = @users.delete(:default)
+      end
+
+      def user_list
+        if @users.empty?
+          say 'No users'
+        else
+          @users.each do |screen_name, config|
+            if screen_name == @default
+              say "* #{screen_name}"
+            else
+              say "  #{screen_name}"
+            end
+          end
+          say "(#{@users.size} users)"
+        end
+      end
+    end
+  end
+end
