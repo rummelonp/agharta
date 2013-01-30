000fd3e1534f68f91ff1ee3b12d220d9626f9e21
Kazuya Takeshima <mail@mitukiii.jp>
2013-01-31 01:56:14 +0900

    Add simplecov groups configuration.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,12 @@
 unless ENV['CI']
   require 'simplecov'
   SimpleCov.start do
-    add_filter 'spec'
+    add_group 'Agharta' do |source|
+      source.filename =~ /lib\/agharta(|\/[^\/]+).rb/
+    end
+    add_group 'Tasks',      'lib/agharta/tasks'
+    add_group 'UsetStream', 'lib/agharta/user_stream'
+    add_group 'Handlers',   'lib/agharta/handlers'
   end
 end
 
