fdf7ed73c47ec229865bfc173ac0d7253a5ffe43
Kazuya Takeshima <mail@mitukiii.jp>
2013-02-02 03:06:22 +0900

    Fix indent.

--- lib/agharta/user_stream/hooks.rb
+++ lib/agharta/user_stream/hooks.rb
@@ -1,32 +1,32 @@
 # -*- coding: utf-8 -*-
 
 module Agharta
-    module UserStream
-      module Hooks
-        def hooks
-          @hooks ||= []
-        end
+  module UserStream
+    module Hooks
+      def hooks
+        @hooks ||= []
+      end
 
-        def keyword(*args, &block)
-          hook = Keyword.new(self, *args, &block)
-          hooks << hook
-          hook
-        end
+      def keyword(*args, &block)
+        hook = Keyword.new(self, *args, &block)
+        hooks << hook
+        hook
+      end
 
-        def user(*args, &block)
-          hook = User.new(self, *args, &block)
-          hooks << hook
-          hook
-        end
+      def user(*args, &block)
+        hook = User.new(self, *args, &block)
+        hooks << hook
+        hook
+      end
 
-        def event(*args, &block)
-          hook = Event.new(self, *args, &block)
-          hooks << hook
-          hook
-        end
+      def event(*args, &block)
+        hook = Event.new(self, *args, &block)
+        hooks << hook
+        hook
       end
     end
   end
+end
 
 require 'agharta/user_stream/hooks/hook'
 require 'agharta/user_stream/hooks/keyword'
