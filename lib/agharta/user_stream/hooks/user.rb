7a7987a654036525af0ffd95b130ab883c883372
Kazuya Takeshima <mail@mitukiii.jp>
2013-02-11 18:38:46 +0900

    Add user hook.

--- lib/agharta/user_stream/hooks/user.rb
+++ lib/agharta/user_stream/hooks/user.rb
@@ -4,6 +4,33 @@ module Agharta
   module UserStream
     module Hooks
       class User < Hook
+        def initialize(context, *args, &block)
+          options = args.last.is_a?(Hash) ? args.pop : {}
+          @include = options.fetch(:include, [])
+          @all     = options.fetch(:all,     false)
+          instance_eval &block if block_given?
+        end
+
+        def call(status)
+          return unless status[:text]
+          return unless status[:user]
+          user = status[:user][:screen_name]
+          if @all || @include.flatten.any? { |u| user == u }
+            options = {
+              :type => :user,
+              :user => user
+            }
+            handlers.each { |h| h.call(status, options) }
+          end
+        end
+
+        def include(*keywords)
+          @include.concat(keywords)
+        end
+
+        def all!
+          @all = true
+        end
       end
     end
   end
