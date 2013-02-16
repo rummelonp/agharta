397a02319af0bc34e45fb739b139b4ae1bf4e861
Kazuya Takeshima <mail@mitukiii.jp>
2013-02-17 02:50:47 +0900

    Add event validate.

--- lib/agharta/user_stream/hooks/event.rb
+++ lib/agharta/user_stream/hooks/event.rb
@@ -4,8 +4,26 @@ module Agharta
   module UserStream
     module Hooks
       class Event < Hook
+        EVENT_NAMES = [
+          :block,
+          :unblock,
+          :favorite,
+          :unfavorite,
+          :follow,
+          :unfollow,
+          :list_created,
+          :list_destroyed,
+          :list_updated,
+          :list_member_added,
+          :list_member_removed,
+          :list_user_subscribed,
+          :list_user_unsubscribed,
+          :user_update,
+        ].freeze
+
         def initialize(context, *args, &block)
           options = args.last.is_a?(Hash) ? args.pop : {}
+          validate!(args)
           @on = args
           @ignore_self = options.fetch(:ignore_self, false)
           super
@@ -29,11 +47,21 @@ module Agharta
         end
 
         def on(*events)
+          validate!(events)
           @on.concat(events)
         end
 
         def ignore_self!
           @ignore_self = true
+        end
+
+        private
+        def validate!(events)
+          events.each do |event|
+            unless EVENT_NAMES.include?(event)
+              raise ArgumentError, "No such event \"#{event}\""
+            end
+          end
         end
       end
     end
