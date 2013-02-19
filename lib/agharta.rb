f7bd17a94bf9469a72a76be8a4cc86644ea4dfd5
Kazuya Takeshima <mail@mitukiii.jp>
2013-02-19 19:11:46 +0900

    Add recipe new alias method to agharta module.

--- lib/agharta.rb
+++ lib/agharta.rb
@@ -1,21 +1,25 @@
 # -*- coding: utf-8 -*-
 
+require 'agharta/version'
+require 'agharta/errors'
+require 'agharta/core_ext'
+require 'agharta/status_formatter'
+require 'agharta/context'
+require 'agharta/multi_logger'
+require 'agharta/simple_formatter'
+require 'agharta/environment'
+require 'agharta/tasks'
+require 'agharta/executable'
+require 'agharta/configuration'
+require 'agharta/handlers'
+require 'agharta/stores'
+require 'agharta/notifies'
+require 'agharta/user_stream'
+require 'agharta/recipe'
+require 'agharta/file_recipe'
+
 module Agharta
-  require 'agharta/version'
-  require 'agharta/errors'
-  require 'agharta/core_ext'
-  require 'agharta/status_formatter'
-  require 'agharta/context'
-  require 'agharta/multi_logger'
-  require 'agharta/simple_formatter'
-  require 'agharta/environment'
-  require 'agharta/tasks'
-  require 'agharta/executable'
-  require 'agharta/configuration'
-  require 'agharta/handlers'
-  require 'agharta/stores'
-  require 'agharta/notifies'
-  require 'agharta/user_stream'
-  require 'agharta/recipe'
-  require 'agharta/file_recipe'
+  def self.new(name  = caller.first.split(':').first)
+    Recipe.new(name)
+  end
 end
